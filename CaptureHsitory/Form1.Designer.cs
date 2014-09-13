fcff9f6118f167edde16623240157e9a5c13dda2
GHat <issacs98@gmail.com>
2014-09-13 20:07:25 +0900

    initalize
    initalize

---
+++ CaptureHsitory/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CaptureHsitory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(64, 64);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(439, 328);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 328);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Capture History";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ CaptureHsitory/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureHsitory
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Directory.CreateDirectory("images");
+                Directory.CreateDirectory(@"images\"+DateTime.Now.ToString("yyyy년MM월dd일"));
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.ToString());
+            }
+            ClipboardNotification.ClipboardUpdate += ClipboardNotification_ClipboardUpdate;
+        }
+
+        void ClipboardNotification_ClipboardUpdate(object sender, EventArgs e)
+        {
+            Image img = Clipboard.GetImage();
+            imageList1.Images.Add(img);
+            String ntime = System.DateTime.Now.ToString("yyyy-MM-dd HH시mm분ss초ms");
+
+
+            img.Save(@"images\" + DateTime.Now.ToString("yyyy년MM월dd일") + @"\"+ ntime + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            ListViewItem lvi = new ListViewItem(ntime);
+            
+            lvi.ImageIndex = 0;
+            listView1.Items.Add(lvi);
+           
+        }
+
+        /*
+         * ------------------------------------------------
+         * ClipboardNotification Class from StackOverFlow
+         * ------------------------------------------------
+         * */
+        public sealed class ClipboardNotification
+        {
+            /// <summary>
+            /// Occurs when the contents of the clipboard is updated.
+            /// </summary>
+            public static event EventHandler ClipboardUpdate;
+
+            private static NotificationForm _form = new NotificationForm();
+
+            /// <summary>
+            /// Raises the <see cref="ClipboardUpdate"/> event.
+            /// </summary>
+            /// <param name="e">Event arguments for the event.</param>
+            private static void OnClipboardUpdate(EventArgs e)
+            {
+                var handler = ClipboardUpdate;
+                if (handler != null)
+                {
+                    handler(null, e);
+                }
+            }
+
+            /// <summary>
+            /// Hidden form to recieve the WM_CLIPBOARDUPDATE message.
+            /// </summary>
+            private class NotificationForm : Form
+            {
+                public NotificationForm()
+                {
+                    NativeMethods.SetParent(Handle, NativeMethods.HWND_MESSAGE);
+                    NativeMethods.AddClipboardFormatListener(Handle);
+                }
+
+                protected override void WndProc(ref Message m)
+                {
+                    if (m.Msg == NativeMethods.WM_CLIPBOARDUPDATE)
+                    {
+                        OnClipboardUpdate(null);
+                    }
+                    base.WndProc(ref m);
+                }
+            }
+        }
+
+        internal static class NativeMethods
+        {
+            // See http://msdn.microsoft.com/en-us/library/ms649021%28v=vs.85%29.aspx
+            public const int WM_CLIPBOARDUPDATE = 0x031D;
+            public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+
+            // See http://msdn.microsoft.com/en-us/library/ms632599%28VS.85%29.aspx#message_only
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+            // See http://msdn.microsoft.com/en-us/library/ms633541%28v=vs.85%29.aspx
+            // See http://msdn.microsoft.com/en-us/library/ms649033%28VS.85%29.aspx
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2(this);
+            frm2.Text = listView1.FocusedItem.Text;
+            Image img = Image.FromFile(@"images\" + DateTime.Now.ToString("yyyy년MM월dd일") +@"\"+ listView1.FocusedItem.Text + ".png");
+            frm2.pictureBox1.Image = img;
+            frm2.Show();
+        }
+    }
+}
